51b2074725956d5942623556e667adee4490f550
Appy-arsnel <75736454+Appy-arsnel@users.noreply.github.com>
2021-06-01 04:44:15 +0530

    opening app screens

--- app/src/main/java/com/example/mallapp/MainActivity.java
+++ app/src/main/java/com/example/mallapp/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.mallapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // creating object of ViewPager
+        ViewPager mViewPager;
+        ProgressBar progressBar;
+        // images array
+        int[] images = {R.drawable.a1, R.drawable.a2, R.drawable.a3, R.drawable.a4,R.drawable.a5,R.drawable.a6};
+
+        // Creating Object of ViewPagerAdapter
+        ViewPagerAdapter mViewPagerAdapter;
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            // Initializing the ViewPager Object
+            mViewPager = (ViewPager)findViewById(R.id.viewPagerMain);
+
+            // Initializing the ViewPagerAdapter
+            mViewPagerAdapter = new ViewPagerAdapter(MainActivity.this, images);
+
+            // Adding the Adapter to the ViewPager
+            mViewPager.setAdapter(mViewPagerAdapter);
+        }
     }
-}
