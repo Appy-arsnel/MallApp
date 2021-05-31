824ed67c74d467de71391613deaef3a66d589a47
Appy-arsnel <75736454+Appy-arsnel@users.noreply.github.com>
2021-06-01 00:53:18 +0530

    splash screen improvement 1.0

--- app/src/main/java/com/example/mallapp/splashscreen.java
+++ app/src/main/java/com/example/mallapp/splashscreen.java
@@ -6,20 +6,31 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.VideoView;
 
 public class splashscreen extends AppCompatActivity {
     private final int SPLASH_DISPLAY_LENGTH = 4000;
     private VideoView video;
+    private ImageView image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
         video= findViewById(R.id.splashs);
+        image= findViewById(R.id.logo);
 
         Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.splassh);
         video.setVideoURI(uri);
-        video.start();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                image.setVisibility(View.INVISIBLE);
+                video.start();
+            }
+        },3000);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
